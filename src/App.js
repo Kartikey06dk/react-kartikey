39444297ed624ae54582312ae527c48cfe0d4824
Kartikey06dk <kartikeysingh253@gmail.com>
2023-07-20 01:02:09 +0530

    hell

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+const App=()=>{
+
+    return (
+       <div>
+        <h1>Smart an beauty together</h1>
+        Hello World ! 
+       </div> 
+    )
+}
+export default App;
